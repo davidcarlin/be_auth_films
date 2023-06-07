53ca1a34c4b5199d7ae121882ae8dd1c9a5606b8
davidcarlin <david.carlin@barclays.com>
2023-06-07 05:45:39 +0100

    config & auth

---
+++ src/main/java/com/example/be_auth_films/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.example.be_auth_films.auth;
+
+import com.example.be_auth_films.user.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
